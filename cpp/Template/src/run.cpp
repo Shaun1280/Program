4451fee6f1e715e15f9187a8cb9dbcc3bee51afa
Shaun1280 <jpanau@connect.ust.hk>
2024-09-19 21:25:08 +0800

    Update run.cpp

--- cpp/Template/src/run.cpp
+++ cpp/Template/src/run.cpp
@@ -1,5 +1,11 @@
+#include <MetaNN/data/facilities/continuous_memory.h>
 #include <MetaNN/data/facilities/tags.h>
 #include <MetaNN/data/facilities/traits.h>
 #include <MetaNN/data/matrices/matrices.h>
 
-int main() { return 0; }
+using CPU = MetaNN::DeviceTags::CPU;
+
+int main() {
+    MetaNN::ContinousMemory<int, CPU> memory(10);
+    return 0;
+}
