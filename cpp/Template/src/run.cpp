c57d5ca8b18dac4a99bd8318e7e375733d2efc5f
Shaun1280 <jpanau@connect.ust.hk>
2024-09-28 22:17:50 +0800

    Update run.cpp

--- cpp/Template/src/run.cpp
+++ cpp/Template/src/run.cpp
@@ -1,4 +1,5 @@
 #include <MetaNN/data/facilities/continuous_memory.h>
+#include <MetaNN/data/facilities/lower_access.h>
 #include <MetaNN/data/facilities/tags.h>
 #include <MetaNN/data/facilities/traits.h>
 #include <MetaNN/data/matrices/cpu_matrix.h>
@@ -9,5 +10,6 @@ using CPU = MetaNN::DeviceTags::CPU;
 int main() {
     MetaNN::ContinousMemory<int, CPU> memory(10);
     MetaNN::Matrix<int, CPU> matrix(3, 3);
+    auto la = LowerAccess(matrix);
     return 0;
 }
