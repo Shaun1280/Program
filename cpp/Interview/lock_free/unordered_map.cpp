ecd606c2ecead36d17fd8fbeac77001b8c321143
Shaun1280 <jpanau@connect.ust.hk>
2024-10-03 22:40:06 +0800

    updated

--- cpp/Interview/lock_free/unordered_map.cpp
+++ cpp/Interview/lock_free/unordered_map.cpp
@@ -1,27 +1,10 @@
 #include <atomic>
 #include <iostream>
-#include <shared_mutex>
-#include <unordered_map>
 #include <vector>
 
-template <typename Key, typename Value> class LockFreeHashMap {
-  private:
-    struct Node {
-        Key key;
-        Value value;
-        std::atomic<Node*> next;
-    };
-
-    std::vector<std::atomic<Node*>> buckets;
-    std::shared_mutex mutex;
-
-    // Hash函数
-    size_t hash(const Key& key) {
-        return std::hash<Key>{}(key) % buckets.size();
-    }
-
+template <typename Key, typename Value> class unordered_map {
   public:
-    LockFreeHashMap(size_t num_buckets) : buckets(num_buckets) {}
+    explicit unordered_map(size_t num_buckets) : buckets(num_buckets) {}
 
     void insert(const Key& key, const Value& value) {
         size_t idx = hash(key);
@@ -30,7 +13,9 @@ template <typename Key, typename Value> class LockFreeHashMap {
         Node* head = buckets[idx];
         new_node->next = head;
 
+        // head is expected value, new_node is desired value
         while (!buckets[idx].compare_exchange_weak(head, new_node)) {
+            // if fail to swap, head will be updated to the current value
             new_node->next = head;
         }
     }
@@ -49,10 +34,34 @@ template <typename Key, typename Value> class LockFreeHashMap {
 
         return false;
     }
+
+    ~unordered_map() {
+        for (size_t i = 0; i < buckets.size(); ++i) {
+            Node* current = buckets[i];
+            while (current) {
+                Node* next = current->next;
+                delete current;
+                current = next;
+            }
+        }
+    }
+
+  private:
+    struct Node {
+        Key key;
+        Value value;
+        std::atomic<Node*> next;
+    };
+
+    std::vector<std::atomic<Node*>> buckets;
+
+    size_t hash(const Key& key) {
+        return std::hash<Key>{}(key) % buckets.size();
+    }
 };
 
 int main() {
-    LockFreeHashMap<int, std::string> map(10);
+    unordered_map<int, std::string> map(10);
 
     map.insert(1, "One");
     map.insert(2, "Two");
