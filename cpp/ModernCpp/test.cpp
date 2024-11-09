255bf25430b4d58baac0412926c592d5d8232de9
Shaun1280 <jpanau@connect.ust.hk>
2024-11-09 18:01:53 +0800

    update

--- cpp/ModernCpp/test.cpp
+++ cpp/ModernCpp/test.cpp
@@ -2,36 +2,77 @@
 
 using namespace std;
 
-class Solution {
-  public:
-    long long maxScore(vector<int>& nums) {
-        if (nums.size() == 1)
-            return 1LL * nums[0] * nums[0];
-        long long ans = 0;
-        for (int skip = -1; skip < (int)nums.size(); skip++) {
-            long long _gcd = 0, lcm = 0;
-            // cout << "skip: " << skip << endl;
-            for (int i = 0; i < nums.size(); i++) {
-                if (i == skip)
-                    continue;
-                if (!_gcd) {
-                    _gcd = nums[i];
-                    lcm = nums[i];
-                } else {
-                    _gcd = gcd(_gcd, nums[i]);
-                    lcm = lcm * nums[i] / gcd(lcm, nums[i]);
-                }
-            }
-            // cout << _gcd << " " << lcm << endl;
-            ans = max(ans, _gcd * lcm);
-        }
-        return ans;
-    }
-};
-
 int main() {
-    Solution sln;
-    vector<int> nums = {2, 4, 8, 16};
-    cout << sln.maxScore(nums) << endl;
+    std::ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int n, k;
+
+    cin >> n >> k;
+    vector<string> _map(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> _map[i];
+        _map[i].insert(_map[i].begin(), ' ');
+    }
+
+    vector<pair<int, int>> q;
+    vector<vector<int>> sum_a(n + 1, vector<int>(n + 1));
+    vector<vector<int>> vis(n + 1, vector<int>(n + 1));
+
+    int longest = 0;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            sum_a[i][j] =
+                max(sum_a[i - 1][j], sum_a[i][j - 1]) + (_map[i][j] == 'a');
+            // cout << sum_a[i][j] << " ";
+            if (i + j - 1 <= sum_a[i][j] + k) {
+                longest = max(longest, i + j - 1);
+            }
+        }
+        // cout << "\n";
+    }
+
+    // cout << longest << "\n";
+
+    string s;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (i + j - 1 <= sum_a[i][j] + k && i + j - 1 == longest) {
+                q.push_back({i, j});
+            }
+        }
+    }
+
+    // cout << "ok" << "\n";
+
+    if (longest == 0) {
+        s.push_back(_map[1][1]);
+        q.push_back({1, 1});
+    }
+
+    while (q.size()) {
+        decltype(q) tmp;
+        char mn = 'z' + 1;
+        for (const auto& [i, j] : q) {
+            if (i + 1 <= n && _map[i + 1][j] < mn)
+                mn = _map[i + 1][j];
+            if (j + 1 <= n && _map[i][j + 1] < mn)
+                mn = _map[i][j + 1];
+        }
+
+        if (mn != 'z' + 1)
+            s.push_back(mn);
+        for (const auto& [i, j] : q) {
+            if (i + 1 <= n && _map[i + 1][j] == mn && !vis[i + 1][j]) 
+                tmp.push_back({i + 1, j}), vis[i + 1][j] = 1;
+            if (j + 1 <= n && _map[i][j + 1] == mn && !vis[i][j + 1])
+                tmp.push_back({i, j + 1}), vis[i][j + 1] = 1;
+        }
+
+        q = std::move(tmp);
+    }
+
+    for (int i = 0; i < longest; ++i)
+        cout << "a";
+    cout << s << "\n";
+
     return 0;
 }
---
+++ cpp/Template/include/MetaNN/operators/tanh_derivative.h
@@ -0,0 +1,18 @@
+#ifndef METANN_OPERATORS_TANH_DERIVATIVE_H
+#define METANN_OPERATORS_TANH_DERIVATIVE_H
+
+#include <MetaNN/data/batch/duplicate.h>
+#include <MetaNN/data/facilities/traits.h>
+#include <MetaNN/data/matrices/trivial_matrix.h>
+#include <MetaNN/facilities/traits.h>
+#include <MetaNN/operators/facilities/tags.h>
+#include <MetaNN/operators/facilities/traits.h>
+#include <MetaNN/operators/operators.h>
+
+namespace MetaNN {
+
+
+
+} // namespace MetaNN
+
+#endif
