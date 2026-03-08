3309adda30a5b8a7d7239be1bd1fa53e14f8f2dc
jpan <jpanau@connect.ust.hk>
2026-03-08 21:02:39 +0800

    rename executable to smart_pointers_test and update test implementation

--- cpp/Interview/smart_pointers/test.cpp
+++ cpp/Interview/smart_pointers/test.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
+#include <shared_ptr.hpp>
+#include <weak_ptr.hpp>
+
 int main()
 {
+    shared_ptr<int> sp1( new int( 42 ) );
+    weak_ptr sp2( sp1 );
 
+    std::cout << sp2.expired() << std::endl;
+    return 0;
 }
